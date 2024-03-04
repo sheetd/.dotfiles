27bcf9317f9f12f06a6b93b4a64b83a09a95d69c
sheetd <neil@sheetd.com>
2024-03-03 20:49:54 -0500

    working version

--- init.py
+++ init.py
@@ -1,30 +1,71 @@
 import os
 
-# Created symbolic links for the files/folders in the .dotfiles directory
+# Creates symbolic links for the files/folders in the .dotfiles directory
+
+# function to create links
 def create_sym_link(source, destination):
   try:
-    #os.symlink(source, destination)
-    print("Link created:", source, "--> ", destination)
+    os.symlink(source, destination)
+    print("✅ Link created:", source, "--> ", destination)
   except FileExistsError:
-    print("File already exists:", destination)
+    print("🛑 File already exists:", destination)
 
 # Simple Function
 #path1 = "/Users/neil/.dotfiles-mac/.gitconfig"
 #path2 = "/Users/neil/.gitconfig"
 #create_sym_link(path1, path2)
 
-
-# ---------------------------------------------------------
-# Function using Tuple
+# using Tuple
 paths = ("/Users/neil/.dotfiles-mac/.gitconfig", "/Users/neil/.gitconfig", \
-"/Users/neil/.dotfiles-mac/.zshrc", "/Users/neil/.zshrc")
+"/Users/neil/.dotfiles-mac/.zshrc", "/Users/neil/.zshrc", \
+"/Users/neil/.dotfiles-mac/.ssh", "/Users/neil/.ssh", \
+"/Users/neil/.dotfiles-mac/Brewfile", "/Users/neil/.Brewfile", \
+"/Users/neil/.dotfiles-mac/.aws", "/Users/neil/.aws" \
+)
+
+# path1 = paths[0]
+# path2 = paths[1]
+# print(path1, " --> ", path2)
+
+# print(paths)
+# print(paths[0::2])
+# print(paths[1::2])
+
+# for i in range(0, 10, 2):
+#   print(i)
+
+print("Creating links:")
+for i in range(0, len(paths), 2):
+  path1 = paths[i]
+  path2 = paths[i+1]
+  # print(path1, " --> ", path2)
+  create_sym_link(path1, path2)
+
+
+
+# TODO: setup variables for the path prefixes like
+
+path_home = "/Users/neil"
+path_repo = "/Users/neil/.dotfiles-mac"
+
+
+# -------
+
 
 '''
-# how to increment this?
+# using a loop
 for path in paths:
-  print("Link created:", paths[0], "--> ", paths[1])
-'''
+  # 1, 3, 5...
+  path1 = path
 
+  # 2, 4, 6...
+  index = paths.index(path)
+  index += 1
+  path2 = paths.index(index)
+
+  print(path1, " --> ", path2)
+
+  
 # try incrementing another way
 for i in paths:
   print(paths.index(i))
@@ -35,14 +76,14 @@ for i in paths:
   path2 = paths[i+1]
   print(path2)
 
-
-'''
 for i in range(len(paths)):
   print(i)
 
 size = len(paths)
 for i in range(size):
   print(i)
+
+
+# get a range in a list
+print(paths[0:2])
 '''
-
-
